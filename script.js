e22c493e1a2b155da8dfdeb3d97f0842314ca0cd
Andrew Bonas <aabonas@hotmail.com>
2021-01-13 09:58:55 -0800

    library book cards added

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const cardsCtn = document.getElementById('cards-ctn')
 let myLibrary =[];
 
 function Book(title, author, pages, read) {
@@ -14,5 +15,55 @@ function addBookToLibrary(title, author, pages, read) {
 
 
 addBookToLibrary('The Hobbit', 'JRR Tolkien', 500, true);
-
+addBookToLibrary('The Hobbit', 'JRR Tolkien', 500, false);
 console.log(myLibrary);
+
+
+function render() {
+  myLibrary.forEach((book) => {
+    let bookCard = document.createElement('div')
+    bookCard.classList.add('card');
+    cardsCtn.prepend(bookCard);
+
+    let titleInfo = document.createElement('h1');
+    titleInfo.classList.add('card-header');
+    titleInfo.textContent = book.title;
+    bookCard.appendChild(titleInfo);
+
+
+    let authorInfo = document.createElement('h5');
+    authorInfo.classList.add('card-author');
+    authorInfo.textContent = 'By: ' + book.author;
+    bookCard.appendChild(authorInfo);
+
+    let pagesInfo = document.createElement('p');
+    pagesInfo.classList.add('card-pages');
+    pagesInfo.textContent = book.pages + ' pages';
+    bookCard.appendChild(pagesInfo);
+
+    let readStatus = document.createElement('p')
+    readStatus.classList.add('readStatus');
+    bookCard.appendChild(readStatus);
+
+
+    bookComplete = () => {
+      readStatus.style.color = 'rgb(13, 228, 167)';
+      readStatus.textContent = 'Complete';
+    };
+
+    bookInComplete = () => {
+      readStatus.style.color = 'rgb(222, 50, 12)';
+      readStatus.textContent = 'Not read';
+    };
+
+    if(book.read == true) {
+      bookComplete();
+    } else {
+      bookInComplete();
+    };
+
+
+});
+}
+
+render();
