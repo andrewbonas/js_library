14d691643187e312e36e13c545797f1f2a5534a3
Andrew Bonas <aabonas@hotmail.com>
2021-01-15 14:41:24 -0800

    local storage added

--- script.js
+++ script.js
@@ -5,8 +5,11 @@ const cancelBtn = document.querySelector('.cancel-btn');
 const form = document.getElementById('form');
 const formCtn = document.getElementById('form-container');
 
+// Array holds books
 let myLibrary = [];
+bookLibrary = JSON.parse(localStorage.getItem("myLibrary") || "[]");
 
+//create book properties
 function Book(id, title, author, pages, read) {
   this.id = id;
   this.title = title;
@@ -19,14 +22,10 @@ function addBookToLibrary(title, author, pages, read) {
   newBook = new Book(title, author, pages, read);
   myLibrary.push(newBook);
 }
-
-
-addBookToLibrary(1606253782720, 'The Hobbit', 'JRR Tolkien', 500, true);
-addBookToLibrary(1606253782720, 'The Hobbit', 'JRR Tolkien', 500, true);
+//pre-loaded example
 addBookToLibrary(1606253782720, 'The Hobbit', 'JRR Tolkien', 500, true);
 
-
-
+//create library, books displayed on cards
 function render() {
   myLibrary.forEach((book) => {
     let bookCard = document.createElement('div')
@@ -54,7 +53,7 @@ function render() {
     readStatus.setAttribute('unique-id', book.id);
 
     bookComplete = () => {
-      readStatus.style.color = 'rgb(13, 228, 167)';
+      readStatus.style.color = 'rgba(113, 247, 49, 0.94)';
       readStatus.textContent = 'Complete';
     };
 
@@ -76,9 +75,9 @@ function render() {
     bookCard.appendChild(bookDelete);
   });
 }
-
 render();
 
+//open and close form with button
 function openForm() {
   document.getElementById("form-container").style.display = "block";
 }
@@ -87,13 +86,23 @@ function closeForm() {
   document.getElementById("form-container").style.display = "none";
 }
 
-function submitFrom() {
+//user submits form to add book to library
+function submitForm() {
+  let title = document.getElementById('book-name').value;
+  let author = document.getElementById('author-name').value;
+  let pages = document.getElementById('book-pages').value;
+  if ( title == "" || author == "" || pages < 1 || pages > 20000) {
+    alert('Please fill out correctly')
+  } else {
+  document.getElementById('book-pages').value;
+
   addBook();
-  console.log(myLibrary);
   formCtn.style.display = 'none';
   form.reset();
 }
+}
 
+//the form submits
 function addBook() {
   this.id = Date.now();
   this.title = document.getElementById('book-name').value;
@@ -101,43 +110,47 @@ function addBook() {
   this.pages = document.getElementById('book-pages').value;
   this.read = document.getElementById('book-status').checked;
 
-  let addNewBook = new Book(idtitle, author, pages, read);
+  let addNewBook = new Book(id, title, author, pages, read);
   myLibrary.push(addNewBook);
 
   cards.querySelectorAll('.card').forEach(e => e.remove());
+  localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
   render();
-
 }
 
-cards.addEventListener('click', e => {
-  let getId = e.target.getAttribute('unique-id');
-  if (e.target.classList.contains('book-del-btn')) {
-    for (let i = 0; i < myLibrary.length; i++) {
-      if (myLibrary[i].id == getId) {
-        myLibrary.splice(i, 1);
-      };
-    };
-    let rmv = e.target.parentElement;
-    cards.removeChild(rmv);
-
-  };
-  if (e.target.classList.contains('read-status')) {
-    for (let i = 0; i < myLibrary.length; i++) {
-      if (myLibrary[i].id == getId) {
-        if (myLibrary[i].read == true) {
-          myLibrary[i].read = false;
-          e.target.style.color = 'red';
-          e.target.textContent = 'Not Read';
-        } else {
-          myLibrary[i].read = true;
-          e.target.style.color = 'green';
-          e.target.textContent = 'Complete';
+//delete and update if the book has been read
+function adjustBook() {
+  cards.addEventListener('click', e => {
+    let getId = e.target.getAttribute('unique-id');
+    if (e.target.classList.contains('book-del-btn')) {
+      for (let i = 0; i < myLibrary.length; i++) {
+        if (myLibrary[i].id == getId) {
+          myLibrary.splice(i, 1);
         };
       };
-};
-  };
-});
+      let rmv = e.target.parentElement;
+      cards.removeChild(rmv);
+      localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
+    };
 
+    if (e.target.classList.contains('read-status')) {
+      for (let i = 0; i < myLibrary.length; i++) {
+        if (myLibrary[i].id == getId) {
+          if (myLibrary[i].read == true) {
+            myLibrary[i].read = false;
+            e.target.style.color = 'rgb(222, 50, 12)';
+            e.target.textContent = 'Not Read';
+          } else {
+            myLibrary[i].read = true;
+            e.target.style.color = 'rgba(113, 247, 49, 0.94)';
+            e.target.textContent = 'Complete';
+          };
+          localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
+        };
+      };
+    };
+  });
+}
+adjustBook();
 
-
-console.log(myLibrary)
+console.log(myLibrary);
